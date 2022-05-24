acb9d8edd430cf46aa9bc67f24022ee27a20a849
Guy Farley <guy.e.farley@gmail.com>
2022-05-23 17:05:55 -0700

    Completed back end tasks

--- server.js
+++ server.js
@@ -4,15 +4,49 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL);
+
+const Book = require('./models/books.js');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome!');
+})
+
+app.get('/books', getBooks);
+
+async function getBooks(req, res, next) {
+  let bookQuery = {};
+  if (req.query.title) {
+    bookQuery = {
+      title: req.query.title
+    }
+  }
+  try {
+    let results = await Book.find(bookQuery);
+    res.status(200).send(results);
+  } catch (error) {
+    next(error);
+  }
+}
+
+app.get('*', (request, response) => {
+  response.status(404).send('Not availabe');
+})
 
 app.get('/test', (request, response) => {
-
   response.send('test request received')
-
 })
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
